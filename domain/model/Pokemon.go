86db19e3d1491815af5516354f438892c42ecee9
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-03-24 13:49:48 -0500

    fix(pokemon): for pokemon model, change the var from typeId string to pokemonType Type(struct)

--- domain/model/Pokemon.go
+++ domain/model/Pokemon.go
@@ -1,12 +1,12 @@
 package model
 
 type Pokemon struct {
-	ID     int    `json:"id"`
-	Number int    `json:"number"`
-	Name   string `json:"name"`
-	TypeId int    `json:"type_id"`
+	ID          int    `json:"id"`
+	Number      int    `json:"number"`
+	Name        string `json:"name"`
+	PokemonType Type   `json:"type"`
 }
 
-func NewPokemon(ID int, number int, name string, typeId int) *Pokemon {
-	return &Pokemon{ID: ID, Number: number, Name: name, TypeId: typeId}
+func NewPokemon(ID int, number int, name string, pokemonType Type) *Pokemon {
+	return &Pokemon{ID: ID, Number: number, Name: name, PokemonType: pokemonType}
 }
