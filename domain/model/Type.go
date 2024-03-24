e078504932c9fa43a2e322909d730abf10d64085
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-03-23 23:58:41 -0500

    feat(type): add a model into domain for pokemon type

---
+++ domain/model/Type.go
@@ -0,0 +1,12 @@
+package model
+
+// Type represents a Pokemon type.
+type Type struct {
+	ID         int    `json:"id"`
+	FirstType  string `json:"first_type"`
+	SecondType string `json:"second_type"`
+}
+
+func NewType(ID int, firstType string, secondType string) *Type {
+	return &Type{ID: ID, FirstType: firstType, SecondType: secondType}
+}
