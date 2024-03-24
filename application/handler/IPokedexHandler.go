92c382af5244692383c6c3439d7793438a59ce7b
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-03-24 16:47:34 -0500

    feat(pokedex): add missing methods to handler for pokedex, also add httpstatus code and handling domain errors

--- application/handler/IPokedexHandler.go
+++ application/handler/IPokedexHandler.go
@@ -6,9 +6,9 @@ import (
 )
 
 type IPokedexHandler interface {
-	SavePokemonToPokedex(pokedex *request.Pokedex) error
-	GetAllPokemonFromPokedex() ([]*response.Pokedex, error)
-	GetPokemonFromPokedexByNumber(pokedexNumber int) (*response.Pokedex, error)
-	UpdatePokemonInPokedex(pokedex *request.Pokedex) error
-	DeletePokemonFromPokedex(pokedexNumber int) error
+	SavePokemonToPokedex(pokedex *request.Pokedex) (httpStatus int, err error)
+	GetAllPokemonFromPokedex() (response []*response.Pokedex, httpStatus int, err error)
+	GetPokemonFromPokedexByNumber(pokedexNumber int) (response *response.Pokedex, httpStatus int, err error)
+	UpdatePokemonInPokedex(pokedex *request.Pokedex) (httpStatus int, err error)
+	DeletePokemonFromPokedex(pokedexNumber int) (httpStatus int, err error)
 }
--- application/handler/PokedexHandler.go
+++ application/handler/PokedexHandler.go
@@ -1,10 +1,13 @@
 package handler
 
 import (
+	"errors"
 	"github.com/redooz/podekex-hexagonal-architecture/application/dto/request"
 	"github.com/redooz/podekex-hexagonal-architecture/application/dto/response"
 	"github.com/redooz/podekex-hexagonal-architecture/application/mapper"
 	"github.com/redooz/podekex-hexagonal-architecture/domain/api"
+	domainError "github.com/redooz/podekex-hexagonal-architecture/domain/error"
+	"net/http"
 )
 
 type PokedexHandler struct {
@@ -12,36 +15,63 @@ type PokedexHandler struct {
 	typeServicePort    api.ITypeServicePort
 }
 
-func (p PokedexHandler) SavePokemonToPokedex(pokedex *request.Pokedex) error {
-	err := p.pokemonServicePort.SavePokemon(mapper.PokedexRequestToModel(pokedex))
-	if err != nil {
-		return err
-	}
-
-	return nil
+func NewPokedexHandler(pokemonServicePort api.IPokemonServicePort, typeServicePort api.ITypeServicePort) *PokedexHandler {
+	return &PokedexHandler{pokemonServicePort: pokemonServicePort, typeServicePort: typeServicePort}
 }
 
-func (p PokedexHandler) GetAllPokemonFromPokedex() ([]*response.Pokedex, error) {
+func (p PokedexHandler) SavePokemonToPokedex(pokedex *request.Pokedex) (httpStatus int, err error) {
+	err = p.pokemonServicePort.SavePokemon(mapper.PokedexRequestToModel(pokedex))
+	if err != nil {
+		return http.StatusInternalServerError, err
+	}
+
+	return http.StatusCreated, nil
+}
+
+func (p PokedexHandler) GetAllPokemonFromPokedex() (response []*response.Pokedex, httpStatus int, err error) {
 	pokedex, err := p.pokemonServicePort.GetAllPokemons()
 
 	if err != nil {
-		return nil, err
+		return nil, http.StatusInternalServerError, err
 	}
 
-	return mapper.SlicePokemonModelToSliceResponse(pokedex), nil
+	var noDataFound *domainError.NoDataFound
+	if errors.As(err, &noDataFound) {
+		return nil, http.StatusNotFound, nil
+	}
+
+	return mapper.SlicePokemonModelToSliceResponse(pokedex), http.StatusOK, nil
 }
 
-func (p PokedexHandler) GetPokemonFromPokedexByNumber(pokedexNumber int) (*response.Pokedex, error) {
-	//TODO implement me
-	panic("implement me")
+func (p PokedexHandler) GetPokemonFromPokedexByNumber(pokedexNumber int) (response *response.Pokedex, httpStatus int, err error) {
+	pokemon, err := p.pokemonServicePort.GetPokemonByNumber(pokedexNumber)
+
+	if err != nil {
+		return nil, http.StatusInternalServerError, err
+	}
+
+	var noDataFound *domainError.NoDataFound
+	if errors.As(err, &noDataFound) {
+		return nil, http.StatusNotFound, nil
+	}
+
+	return mapper.PokedexModelToResponse(pokemon), http.StatusOK, nil
 }
 
-func (p PokedexHandler) UpdatePokemonInPokedex(pokedex *request.Pokedex) error {
-	//TODO implement me
-	panic("implement me")
+func (p PokedexHandler) UpdatePokemonInPokedex(pokedex *request.Pokedex) (httpStatus int, err error) {
+	err = p.pokemonServicePort.UpdatePokemon(mapper.PokedexRequestToModel(pokedex))
+	if err != nil {
+		return http.StatusInternalServerError, err
+	}
+
+	return http.StatusNoContent, nil
 }
 
-func (p PokedexHandler) DeletePokemonFromPokedex(pokedexNumber int) error {
-	//TODO implement me
-	panic("implement me")
+func (p PokedexHandler) DeletePokemonFromPokedex(pokedexNumber int) (httpStatus int, err error) {
+	err = p.pokemonServicePort.DeletePokemon(pokedexNumber)
+	if err != nil {
+		return http.StatusInternalServerError, err
+	}
+
+	return http.StatusNoContent, nil
 }
