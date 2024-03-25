5ac93bdc5caf6375b59129bfca02d110a3f59c61
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-03-24 20:16:22 -0500

    feat(pokedex): add validations to pokedex request dto

--- application/dto/request/Pokedex.go
+++ application/dto/request/Pokedex.go
@@ -1,9 +1,9 @@
 package request
 
 type Pokedex struct {
-	Number int    `json:"number"`
-	Name   string `json:"name"`
-	TypeId int    `json:"type_id"`
+	Number int    `json:"number" validate:"required"`
+	Name   string `json:"name" validate:"required,min=3,max=50"`
+	TypeId int    `json:"type_id" validate:"required"`
 }
 
 func NewPokedex(number int, name string, typeId int) *Pokedex {
