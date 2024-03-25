308a252ebeb489d20949f45fd26a57fee269e58a
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-03-25 15:36:08 -0500

    feat(pokemon): add adapter for pokemon persistence port implementation

---
+++ infrastructure/output/gorm/adapter/PokemonGormAdapter.go
@@ -0,0 +1,65 @@
+package adapter
+
+import (
+	"github.com/redooz/podekex-hexagonal-architecture/domain/model"
+	"github.com/redooz/podekex-hexagonal-architecture/infrastructure/output/gorm/entitymapper"
+	"github.com/redooz/podekex-hexagonal-architecture/infrastructure/output/gorm/repository"
+)
+
+type PokemonPersistenceAdapter struct {
+	Repository repository.Pokemon
+}
+
+func NewPokemonPersistenceAdapter(repository repository.Pokemon) *PokemonPersistenceAdapter {
+	return &PokemonPersistenceAdapter{Repository: repository}
+}
+
+func (p PokemonPersistenceAdapter) SavePokemonToDB(pokemon *model.Pokemon) error {
+	err := p.Repository.Save(entitymapper.PokemonModelToEntity(pokemon))
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p PokemonPersistenceAdapter) GetAllPokemonsFromDB() ([]*model.Pokemon, error) {
+	pokemons, err := p.Repository.GetAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return entitymapper.SlicePokemonEntityToSliceModel(pokemons), nil
+}
+
+func (p PokemonPersistenceAdapter) GetPokemonByNumberFromDB(pokemonNumber int) (*model.Pokemon, error) {
+	pokemon, err := p.Repository.GetByNumber(pokemonNumber)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return entitymapper.PokemonEntityToModel(pokemon), nil
+}
+
+func (p PokemonPersistenceAdapter) UpdatePokemonFromDB(pokemon *model.Pokemon, number int) error {
+	err := p.Repository.Update(entitymapper.PokemonModelToEntity(pokemon), number)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p PokemonPersistenceAdapter) DeletePokemonFromDB(pokemonNumber int) error {
+	err := p.Repository.Delete(pokemonNumber)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
